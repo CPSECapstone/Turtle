74a45a05841079bf3c5b8aab3a14d13e831903cd
Carlos Ayala <11810561+cayalame@users.noreply.github.com>
2020-11-04 14:15:22 -0800

    Initial Home.jsx change

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,11 +1,23 @@
 import React, { Component } from "react";
 
-export default class Home extends Component {
+export default class EmptyPage extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    parser(input) {
+        return <div></div>;
+    }
+
     render() {
+        const { children } = parser(this.props);
+
         return (
             <div>
-                Welcome Home
+                { children }
             </div>
         )
     }
 }
+
+
