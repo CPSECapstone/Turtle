3c5551d033ce14ea6f15d4a000319a2ec8350a20
Joe Soboleski <soboleskijoe@gmail.com>
2020-11-03 16:02:40 -0800

    iIt work. I change code base. Very nice

--- reactjs/src/components/components.js
+++
@@ -1,5 +0,0 @@
-export { default as SignIn } from './SignIn/SignIn';
-export { default as Register } from './Register/RegisterHooks';
-export { default as ConfDialog } from './ConfDialog/ConfDialog';
-export { default as ErrDialog } from './ErrDialog/ErrDialog';
-export { default as Home } from './Home/Home';
--- reactjs/src/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-
-import Prss from './Prss';
-import Errs from './Errs';
-
-const rootReducer = combineReducers({Prss, Errs}); 
-
-export default rootReducer;
