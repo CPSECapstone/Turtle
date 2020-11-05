716d4e0b541319c2ba2694d6bcbc72b4e1bde639
Carlos Ayala <11810561+cayalame@users.noreply.github.com>
2020-11-05 11:52:20 -0800

    commented code

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -5,16 +5,19 @@ export default class EmptyPage extends Component {
         super(props);
     }
 
-    parser(input) {
-        return <div></div>;
-    }
+    // parser(input) {
+    //     return <div>
+    //         <h3>Hello</h3>
+    //     </div>;
+    // }
 
     render() {
-        const { children } = parser(this.props);
+        //const { children } = parser(this.props);
 
         return (
             <div>
-                { children }
+                {/* { children } */}
+                <h3>Hello</h3>
             </div>
         )
     }
