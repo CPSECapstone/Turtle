713bebb59c7f35c46228b162a93c9adb75a9d62f
Carlos Ayala <11810561+cayalame@users.noreply.github.com>
2020-11-12 11:22:47 -0800

    working test table

--- frontend/src/components/NewPage/NewPage.jsx
+++ frontend/src/components/NewPage/NewPage.jsx
@@ -4,12 +4,44 @@ class NewPage extends Component {
 
     constructor(props) {
         super(props);
+        this.state = {
+            students: [
+                { id: 1, name: 'Joe', age: 21, email: 'joe@test.com'},
+                { id: 2, name: 'Bob', age: 22, email: 'Bob@test.com'}
+            ]
+        }
+    }
+
+    returnTableData() {
+        return this.state.students.map((student, index) => {
+            let col = Object.keys(student)
+            return (
+               <tr key={student.id}>
+                  {col.map((val, index) => {
+                     return <td key={index}>{student[col[index]]}</td>
+                  })}
+               </tr>
+            )
+         })
+    }
+
+    renderTableHeader() {
+        let header = Object.keys(this.state.students[0])
+        return header.map((key, index) => {
+            return <th key={index}>{key.toUpperCase()}</th>
+        })
     }
     
     render() {
         return (
             <div>
-                <h3>Welcome Home</h3>
+                <h1 id='title'>TESTING TABLE PAGE</h1>
+                <table id='students'>
+                    <tbody>
+                        <tr>{this.renderTableHeader()}</tr>
+                        {this.returnTableData()}
+                    </tbody>
+                </table>
             </div>
         )
     }
