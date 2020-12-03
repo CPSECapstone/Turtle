b3510468ac03af543c58d4691144effe2801720c
Carlos Ayala <11810561+cayalame@users.noreply.github.com>
2020-12-02 21:51:52 -0800

    removed comments

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -4,19 +4,10 @@ export default class EmptyPage extends Component {
     constructor(props) {
         super(props);
     }
-
-    // parser(input) {
-    //     return <div>
-    //         <h3>Hello</h3>
-    //     </div>;
-    // }
-
     render() {
-        //const { children } = parser(this.props);
 
         return (
             <div>
-                {/* { children } */}
                 <h3>Hello</h3>
             </div>
         )
