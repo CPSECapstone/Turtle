bc68948bbff4794753c4a1c8cf7e7caedaff59a7
Joe Soboleski <soboleskijoe@gmail.com>
2021-03-07 15:38:26 -0800

    Tur 59 - Redo website to fit mocks (#39)
    * all changes
    
    * weird fix

--- frontend/src/pages/Home/Home.styled.js
+++ frontend/src/pages/Home/Home.styled.js
@@ -1,22 +1,7 @@
 import styled from 'styled-components';
-import { palette } from '../../styles/palette';
 
 export const Container = styled.div`
-    margin: 0 32px;
+    margin: 32px 32px 0 32px;
     height: 100%;
     display: flex;
-    flex-direction: column;
 `;
-
-export const Title = styled.h1`
-    color: ${palette.LIGHT_ORANGE};
-    font-size: 32px;
-    font-weight: 700;
-`;
-
-export const LineBreak = styled.div`
-    border: 1px solid ${palette.LIGHT_ORANGE};
-    margin: 64px 0;
-`;
-
-export const Apps = styled.div``;
